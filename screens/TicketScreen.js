e244da2da470fafd2b55c3cc2dbe26b24d9bef17
inezabonte <inezabonte@gmail.com>
2021-01-31 23:52:48 +0200

    ticket page

---
+++ screens/TicketScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import qrcode from '../assets/qr.png'
+const TicketScreen = () => {
+    return (
+        <View style={styles.container}>
+           <Image source={qrcode}></Image> 
+           <Text>Team A Vs Team B</Text>
+           <View>
+            <Text>Names: Ntirenganye Rene Promese</Text>
+            <Text>Date of the event: Sunday 12/05/2021</Text>
+            <Text>Date of purchase: Sunday 12/05/2021</Text>
+            <Text>Address: Kigali Arena</Text>
+            <Text>Seats: 69</Text>
+           </View>
+           <Text>Price: 100FRW</Text>
+           <Button>Print</Button>
+        </View>
+    )
+}
+
+export default TicketScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff'
+    }
+})
