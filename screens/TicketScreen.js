3725a8c0ecf75e7141c7882f2685f855e07421c4
inezabonte <inezabonte@gmail.com>
2021-02-01 09:40:00 +0200

    design ticket screen

--- screens/TicketScreen.js
+++ screens/TicketScreen.js
@@ -9,16 +9,30 @@ const TicketScreen = () => {
             <View style={styles.code}>
                 <Image source={qrcode}></Image> 
             </View>
-           <Text>Team A Vs Team B</Text>
+           <Text style={{fontSize: 24, fontWeight: 'bold'}}>Team A Vs Team B</Text>
            <View>
-            <Text>Names: Ntirenganye Rene Promese</Text>
-            <Text>Date of the event: Sunday 12/05/2021</Text>
-            <Text>Date of purchase: Sunday 12/05/2021</Text>
-            <Text>Address: Kigali Arena</Text>
-            <Text>Seats: 69</Text>
+                <View key={`when`} style={styles.detailsItem}>
+                  <Text style={styles.boldItem}>Date of the event: </Text>
+                  <Text>Sunday 12/05/2021</Text>
+              </View>
+              <View style={styles.detailsItem}>
+                  <Text style={styles.boldItem}>Date of purchase: </Text>
+                  <Text> Sunday 12/05/2021</Text>
+              </View>
+              <View  style={styles.detailsItem}>
+                  <Text style={styles.boldItem}>Address:  </Text>
+                  <Text>Kigali Arena</Text>
+              </View>
+              <View  style={styles.detailsItem}>
+                  <Text style={styles.boldItem}>Seat: </Text>
+                  <Text>69</Text>
+              </View>
+              <View  style={styles.detailsItem}>
+                  <Text style={styles.boldItem}>Price: </Text>
+                  <Text>1000FRW</Text>
+              </View>
            </View>
-           <Text>Price: 100FRW</Text>
-           <Button title='Print' />
+           <Button color="#257AAA" style={{backgroundColor: '#EAF4FB'}} >Print</Button>
         </View>
     )
 }
@@ -30,6 +44,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff'
-    }
+        backgroundColor: '#fff',
+        justifyContent: 'space-evenly'
+    },
+    boldItem:{
+        color: '#257AAA',
+        fontWeight:'bold',
+    },
+    detailsItem:{
+        display:'flex',
+        flexDirection:'row',
+        marginTop:5,
+        paddingLeft:15,
+    },
 })
