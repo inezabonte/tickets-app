782332a2c7f491a51d1fd551312db87edfd8603e
Rene Promesse Ntirenganya <69194903+renepromesse@users.noreply.github.com>
2021-02-07 01:13:05 +0200

    adds the purchased tickets

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,7 @@ import { SafeAreaView, Text, View, ScrollView } from 'react-native';
 import { Card,Title, Paragraph } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { cardContent } from '../dummyData';
-import { headerAction } from '../redux/actions/headerAction';
+import { displayContentAction, headerAction } from '../redux/actions/headerAction';
 import styles from '../styles/Home';
 import MainCard from './MatchCard';
 import TicketCard from './TicketCard';
@@ -13,23 +13,28 @@ export default function Home({navigation}) {
   const dispatch = useDispatch();
   const {homeCardContent} = useSelector(state => state.home);
   const { displayContent } = useSelector(state => state.header);
+  const { ticketCardContent } = useSelector(state => state.home);
   
-  useEffect(()=>{
-    dispatch(headerAction(true));
-  },[])
+  // useEffect(()=>{
+  //   dispatch(headerAction(true));
+  // },[])
   
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.subTitle}>
-        <Title>Upcoming games</Title>
+        <Title>{displayContent === 'Home' ? 'Upcoming games': 'All tickets you bought:'}</Title>
       </View>
       <ScrollView contentContainerStyle={styles.content}>
         { displayContent === 'Home' ?
         homeCardContent.map((content, index) =>(
             <MainCard key={`main-card-${index}`} navigation={navigation} data={{content,index}}/>
-        )) : <Text>No Data yet</Text>
+        )) 
+        : displayContent === 'Purchased' ? 
+        ticketCardContent.map((content, index) =>(
+          <TicketCard key={`main-ticket-card-${index}`} navigation={navigation} data={{content, index}}/>
+        )) : <Text>No data yet</Text>
       }
       </ScrollView>
     </SafeAreaView>
